33b9ad2958b2c858d298d2dd9e3840c3e3a1c743
Felix Fischer <felix@subtract.de>
2014-09-23 14:53:58 +0200

    spin out uitmuntend to its own node package

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
 "use strict";
 
-var request = require('request'),
-    cheerio = require('cheerio'),
+var uitmuntend = require('uitmuntend'),
     fs = require('fs'),
-    handlebars = require('handlebars'),
-    unorm = require('unorm'),
-    iconv = require('iconv-lite');
+    handlebars = require('handlebars');
 
-var endpoint = 'http://www.uitmuntend.de/search.html?search=';
 
-var fetch = function(word) {
-  // re-normalize search string (re-compose decomposed characters) and escape
-  var reqUrl = endpoint + escape(unorm.nfc(word));
-  var options = {'uri': reqUrl, 'encoding': null};
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      parse(iconv.decode(body, 'iso-8859-1'));
-    } else if (error) {
-      log("error fetching " + reqUrl);
-    } else {
-      log(response);
-    }
-  })
-}
-
-var parse = function(body) {
-  var res = {'translations': []};
-  var $ = cheerio.load(body);
-  var trs = $('.t2 tbody tr').filter(function(i, el) {
-    return el.children.length == 5;
-  });
-  for (var i = 0, len = trs.length; i < len; i++) {
-    var tds = $(trs[i]).children();
-    res.translations.push({
-      'original': clean($(tds[0]).text()),
-      'translation': clean($(tds[1]).text())
-    });
+var main = function() {
+  if (typeof process.argv[2] == "string") {
+    uitmuntend.query(process.argv[2], output);
+  } else {
+    throw "error: no valid query string supplied";
   }
-  return output(res);
-};
-
-var clean = function(item) {
-  if (item.substr(item.length-2) == ' |') {
-    item = item.substr(0, item.length-2);
-  };
-  return item;
 }
 
 var output = function(data) {
@@ -57,12 +23,6 @@ var output = function(data) {
       console.log(result);
     }
   });
-}
-
-var main = function() {
-  if (typeof process.argv[2] == "string") {
-    fetch(process.argv[2]);
-  }
 }
 
 if (require.main === module) {
