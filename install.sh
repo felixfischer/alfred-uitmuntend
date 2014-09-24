d456408a311d5a1629698deb4cc4f5a002746729
Felix Fischer <felix@subtract.de>
2014-09-24 16:18:13 +0200

    skip installation when not on OSX

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-echo "Installing Alfred Workflow for uitmuntend.de"
-zip -r uitmuntend.alfredworkflow *.plist *.js *.xml *.png node_modules/
-open uitmuntend.alfredworkflow
+
+if [ "$(uname)" == "Darwin" ]; then
+  if [ -d "/Applications/Alfred 2.app" ]; then
+    echo "Installing Alfred Workflow for uitmuntend.de"
+    zip -qr uitmuntend.alfredworkflow *.plist *.js *.xml *.png node_modules/
+    open uitmuntend.alfredworkflow
+  else
+    echo "Error: Alfred 2 is not installed"
+  fi
+else
+  echo "Installation works only on Mac OS X, not on $(uname)."
+fi
