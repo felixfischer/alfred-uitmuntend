3ffc03c9a844e8635522285d76c216bb1f123d5b
Felix Fischer <felix@subtract.de>
2014-09-28 12:45:45 +0200

    remove libxmljs based tests

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var exec = require('child_process').exec,
-    libxml = require('libxmljs');
+var exec = require('child_process').exec;
 
 var assert = function(condition, msg) {
   if (condition === true) {
@@ -16,17 +15,10 @@ var assert = function(condition, msg) {
 
 var shellCmd = 'node index.js nachschlagen';
 
-var isValidXML = function(str) {
-  try {var xml = libxml.parseXml(str)}
-  catch (e) {return false}
-  finally {return xml.errors.length == 0}
-}
-
 exec(shellCmd, function(error, stdout, stderr) {
   assert(error === null, 'shell.exec("' + shellCmd + '") exits with code 0');
   assert(stdout.indexOf('opzoeken') > -1, 'ouput contains expected word');
   assert(stdout.indexOf('<?xml version="1.0"?>') > -1, 'XML docstring present');
-  assert(isValidXML(stdout), 'XML is parseable');
   console.log("\nGOOD: all tests passed");
 });
 
